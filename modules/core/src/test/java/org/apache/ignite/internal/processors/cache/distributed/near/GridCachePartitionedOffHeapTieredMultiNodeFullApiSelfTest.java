98947a15670288b604660b6d6aede62e7c458628
ivasilinets <ivasilinets@gridgain.com>
2015-04-28 15:30:58 +0300

    #ignite-286: wip.

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedOffHeapTieredMultiNodeFullApiSelfTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/near/GridCachePartitionedOffHeapTieredMultiNodeFullApiSelfTest.java
@@ -17,9 +17,7 @@
 
 package org.apache.ignite.internal.processors.cache.distributed.near;
 
-import org.apache.ignite.*;
 import org.apache.ignite.cache.*;
-import org.apache.ignite.configuration.*;
 
 import static org.apache.ignite.cache.CacheMemoryMode.*;
 
@@ -32,53 +30,8 @@ public class GridCachePartitionedOffHeapTieredMultiNodeFullApiSelfTest extends G
         return 4;
     }
 
-    /** {@inheritDoc} *//*
-    @Override protected NearCacheConfiguration nearConfiguration() {
-        return null;
-    }*/
-
     /** {@inheritDoc} */
     @Override protected CacheMemoryMode memoryMode() {
         return OFFHEAP_TIERED;
-    }
-
-    /**
-     * @throws Exception If failed.
-     */
-    public void testPut() throws Exception {
-        IgniteCache cache = grid(0).cache(null);
-
-        for (int i = 4; i < 5; ++i) {
-            String key = "key_" + 4;
-            System.out.println("key = " + key);
-
-            cache.put(key, i);
-
-            for (int j = 0; j < gridCount(); ++j) {
-                boolean isPrimary = grid(0).affinity(null).isPrimary(grid(j).localNode(), key);
-
-                boolean isBackup = grid(0).affinity(null).isBackup(grid(j).localNode(), key);
-
-                System.out.println("Grid " + j + ", primary=" + isPrimary + ", backup=" + isBackup);
-
-                if (isPrimary) {
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.ONHEAP));
-                    assertNotNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.OFFHEAP));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.SWAP));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.NEAR));
-                    assertNotNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.PRIMARY));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.BACKUP));
-                }
-
-                if (isBackup) {
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.ONHEAP));
-                    assertNotNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.OFFHEAP));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.SWAP));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.NEAR));
-                    assertNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.PRIMARY));
-                    assertNotNull(grid(j).cache(null).withSkipStore().localPeek(key, CachePeekMode.BACKUP));
-                }
-            }
-        }
     }
 }
